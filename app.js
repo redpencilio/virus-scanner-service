f7e03f8a59cf0241cb4dbab82048749c3de3066d
Peter Nowee <peter.nowee@redpencil.io>
2023-10-05 16:14:54 +0200

    Use async and await in GET query route
    h/t cecemel

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ app.get('/', function (req, res) {
   res.send('Hello from virus-scanner-service');
 });
 
-app.get('/virus-scanner/query', function (req, res) {
-  var myQuery = `
-    SELECT *
-    WHERE {
+app.get('/virus-scanner/query', async function (req, res) {
+  const myQuery = `
+    SELECT * WHERE {
       GRAPH <http://mu.semte.ch/graphs/public> {
         ?s ?p ?o.
       }
     }`;
-
-  query(myQuery)
-    .then(function (response) {
-      res.send(JSON.stringify(response));
-    })
-    .catch(function (err) {
-      res.send('Oops something went wrong: ' + JSON.stringify(err));
-    });
+  try {
+    const response = await query(myQuery);
+    res.send(JSON.stringify(response));
+  } catch (err) {
+    res.send('Oops something went wrong: ' + JSON.stringify(err));
+  }
 });
 
 app.post(
