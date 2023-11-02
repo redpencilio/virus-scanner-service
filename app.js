09d665039922b3626844970a7413d4f79f44436a
Peter Nowee <peter.nowee@redpencil.io>
2023-11-02 12:46:16 +0100

    Remove smoke test endpoints

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { NamedNode, triple } from 'rdflib';
 import bodyParser from 'body-parser';
 import { LOG_INCOMING_DELTA } from './config';
-import { app, query, errorHandler } from 'mu';
+import { app, errorHandler } from 'mu';
 import { Delta } from './lib/delta';
 import { existsSync } from 'node:fs';
 import NodeClam from 'clamscan';
@@ -31,37 +31,6 @@ async function hasVirus(path) {
 
 app.get('/', function (req, res) {
   res.send('Hello from virus-scanner-service');
-});
-
-app.get('/virus-scanner/test', async function (req, res) {
-  const testfiles = [
-    '/usr/share/clamav-testfiles/clam.zip',
-    '/usr/share/doc/clamav-base/changelog.gz',
-  ];
-  const expectedresults = [true, false];
-
-  const testresults = await Promise.all(
-    testfiles.map(async (testfile) => await hasVirus(testfile)),
-  );
-
-  const summary = { testfiles, expectedresults, testresults };
-  console.log(summary);
-  res.send(JSON.stringify(summary));
-});
-
-app.get('/virus-scanner/query', async function (req, res) {
-  const myQuery = `
-    SELECT * WHERE {
-      GRAPH <http://mu.semte.ch/graphs/public> {
-        ?s ?p ?o.
-      }
-    }`;
-  try {
-    const response = await query(myQuery);
-    res.send(JSON.stringify(response));
-  } catch (err) {
-    res.send('Oops something went wrong: ' + JSON.stringify(err));
-  }
 });
 
 app.post(
